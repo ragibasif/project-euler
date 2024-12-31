bd6af9af7378a47a07fdf9d9b5268e851c3e0e9c
Ragib Asif <182296466+ragibasif@users.noreply.github.com>
2024-12-31 01:56:14 -0500

    {{UPDATE}} Tue Dec 31 01:56:14 2024

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,12 @@
 module Lib
-    ( someFunc
+    ( multiplesOf3or5
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+-- someFunc :: IO ()
+-- someFunc = putStrLn "someFunc"
+
+multiplesOf3or5 :: Int -> Int
+multiplesOf3or5 n
+    | n < 3 = 0
+    | n `mod` 3 == 0 || n `mod` 5 == 0 = n + multiplesOf3or5 (n-1)
+    | otherwise = multiplesOf3or5 (n-1)
